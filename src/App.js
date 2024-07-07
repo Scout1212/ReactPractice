0ee620fdf73cda32ecb32164f2a83dbe180fee10
Scout1212 <RobertZamora.act@gmail.com>
2024-07-07 16:56:20 -0400

    Doing stuff with states!!!!
    tmr i do more

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
 import Hello from './Components/Hello'
+import Message from "./Components/Message";
 
 function App() {
   return (
@@ -10,9 +11,25 @@ function App() {
         {/*<Greet></Greet>*/}
         {/*<Welcome></Welcome>*/}
         {/*<Hello></Hello>*/}
-        <Greet name = "Bruce" heroName = "Gizmo"></Greet>
-        <Greet name = "Clark" heroName = "Superman"></Greet>
+        {/*<Greet name = "Bruce" heroName = "Gizmo">
+            <p>This is children props</p>
+        </Greet>
+        <Greet name = "Clark" heroName = "Superman">
+            <button>Action</button>
+        </Greet>
         <Greet name = "Diana" heroName = "Wonder Woman"></Greet>
+
+        <Welcome name = "Bruce" heroName = "Gizmo">
+            <p>Banning</p>
+        </Welcome>
+        <Welcome name = "Clark" heroName = "Superman">
+            <p>!!!!!!!</p>
+        </Welcome>
+        <Welcome name = "Diana" heroName = "Wonder Woman"></Welcome>
+        */}
+        <Message>
+            <button>Click</button>
+        </Message>
 
     </div>
   );
--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Greet = (props) => {
+const Greet = props => {
     console.log(props)
-    return <h1>Hello {props.name} my superhero name is {props.heroName}!</h1>
+    return (
+        <div>
+            <h1>
+                Hello {props.name} a.k.a {props.heroName}!
+            </h1>
+            {props.children}
+        </div>
+    )
 }
 
 export default Greet
